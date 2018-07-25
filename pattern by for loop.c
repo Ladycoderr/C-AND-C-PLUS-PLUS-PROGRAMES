a9bddadd013f558715c42691657272d177c7b378
Shivangi Mishra <38549834+Shrutimishra2884@users.noreply.github.com>
2018-07-25 08:51:48 +0530

    Add files via upload

---
+++ pattern by for loop.c
@@ -0,0 +1,14 @@
+#include<stdio.h> //includes the standard input output
+#include<conio.h>//include the console functions
+int main()
+{
+	int i,j;
+	for(i=1;i<11;i++)
+	{
+		for(j=1;j<=i;j++){
+		
+		printf("*");
+	}
+	printf("\n");
+}
+}
